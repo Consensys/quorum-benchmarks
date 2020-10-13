dbbabf830b467cd62266b26daa2b43ad954fcf08
Joshua Fernandes <joshua.fernandes@consensys.net>
2020-10-13 10:55:10 +1000

    removing ganacheCaliper

--- besu/terraform/aws/ibftScale/ganachecaliper/files/caliperProject/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $@ > /tmp/args.txt
-
-GANACHE_VPC_NAME=$1
-GANACHE_AWS_REGION=$2
-
-sed -i "s/PARAM_VPC_NAME/$GANACHE_VPC_NAME/g" $HOME/caliperProject/config/networkconfig.json
-sed -i "s/PARAM_AWS_REGION/$GANACHE_AWS_REGION/g" $HOME/caliperProject/config/networkconfig.json
